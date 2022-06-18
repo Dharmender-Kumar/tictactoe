5cb1bb0420c3d758d505300cbe1f0fb4839b90f7
Dharmender-Kumar <82511641+Dharmender-Kumar@users.noreply.github.com>
2022-06-18 09:57:14 +0530

    finish

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import Square from "./Square";
 
-const board = () => {
+const Board = ({ board, handleSquareClick, winningSquares }) => {
+  const renderSquare = (position) => {
+    const isWinningSquare = winningSquares.includes(position);
+
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={1} />
-        <Square value={1} />
-        <Square value={1} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={1} />
-        <Square value={1} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={1} />
-        <Square value={1} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
 };
 
-export default board;
+export default Board;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
   return (
-    <button type="button" className="square">
+    <button
+      type="button"
+      onClick={onClick}
+      className={`square ${isWinningSquare ? "winning" : ""} ${
+        value === "X" ? "text-green" : "text-orange"
+      }`}
+    >
       {value}
     </button>
   );
